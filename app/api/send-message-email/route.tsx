416d65b0e4a927569c496eec31fd4279b0a9f7a3
VincentAudette <v.audette@audla.ca>
2024-12-10 21:06:49 +0100

    Fixed email sending with @react-email/render

--- app/api/send-message-email/route.tsx
+++ app/api/send-message-email/route.tsx
@@ -2,7 +2,14 @@
 import nodemailer from 'nodemailer';
 import { render } from '@react-email/render';
 import { MessageNotification } from '@/components/emails/MessageNotification';
-import { MessageNotificationProps } from '@/types/email';
+let aws = require("@aws-sdk/client-ses");
+const ses = new aws.SES({
+    credentials: {
+        accessKeyId: process.env.AWS_SES_ACCESS_KEY_ID || '',
+        secretAccessKey: process.env.AWS_SES_SECRET_ACCESS_KEY || '',
+    },
+    region: process.env.AWS_SES_REGION
+});
 
 export async function POST(req: Request): Promise<Response> {
   try {
@@ -16,25 +23,22 @@ export async function POST(req: Request): Promise<Response> {
     const { recipientEmail, senderName, assignmentTitle, message } = body;
 
     // Render the email content
-    const emailHtml = render(
+    const emailHtml = await render(
       <MessageNotification senderName={senderName} assignmentTitle={assignmentTitle} message={message} />
     );
 
     // Configure the Nodemailer transporter with AWS SES
     const transporter = nodemailer.createTransport({
-      SES: {
-        apiVersion: '2010-12-01',
-        region: process.env.AWS_REGION, // Your AWS region
-      },
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-    });
+        SES: { ses, aws },
+        sendingRate: 14 
+    })
 
     // Email options
     const mailOptions = {
-      from: 'your-email@example.com', // Your verified email
+      from: 'no-reply@audla.ca',
       to: recipientEmail,
       subject: `New Message Regarding: ${assignmentTitle}`,
+      text: `You have a new message regarding the assignment: ${assignmentTitle}`,
       html: emailHtml,
     };
 
